475403286183f2279eec505bc23c4223962fe65b
Rene Enriquez <enriquezrene.ap@gmail.com>
2018-03-06 22:41:50 -0500

    Update AzureDemoApplication.java

--- demo-azure/src/main/java/com/distribuida/azure/AzureDemoApplication.java
+++ demo-azure/src/main/java/com/distribuida/azure/AzureDemoApplication.java
@@ -7,17 +7,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//@SpringBootApplication
-//@Docu
+@SpringBootApplication
 public class AzureDemoApplication {
 
-//    public static void main(String[] args) {
-        public static void main(String[] args) {
-            List<Integer> myList =
-                    Arrays.asList(1, 2, 3);
-            int result = myList.stream()
-                    .sum();
-            System.out.println(result);
-        }
-    }
+   public static void main(String[] args) {
+       SpringApplication.run(AzureDemoApplication.class, args);
+   }
 }
