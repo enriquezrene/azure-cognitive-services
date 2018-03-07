deb424bbeebb93576e7027b19ead1b1b946e1264
Rene Enriquez <enriquezrene.ap@gmail.com>
2018-03-06 22:42:06 -0500

    Update AzureDemoApplication.java

--- demo-azure/src/main/java/com/distribuida/azure/AzureDemoApplication.java
+++ demo-azure/src/main/java/com/distribuida/azure/AzureDemoApplication.java
@@ -3,9 +3,6 @@ package com.distribuida.azure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class AzureDemoApplication {
